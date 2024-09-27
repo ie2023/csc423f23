03fa53314fdffaef790de65f196ffd3358e678e9
ie2023 <eiskrenovaekiert@brockport.edu>
2024-09-27 11:21:27 -0400

    textbook example

---
+++ web-storage.js
@@ -0,0 +1,13 @@
+const playerNameWidget      = document.getElementById("playerName");
+const difficultyLevelWidget = document.getElementById("difficultyLevel");
+        
+if (localStorage.getItem("playerName")) {                
+   playerNameWidget.value      = localStorage.getItem("playerName");
+   difficultyLevelWidget.value = localStorage.getItem("difficultyLevel");
+}
+
+
+document.getElementById("saveBtn").addEventListener("click", function() {
+        localStorage.setItem("playerName", playerNameWidget.value);
+        localStorage.setItem("difficultyLevel", difficultyLevelWidget.value);
+    });
